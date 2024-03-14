99a2130efa163ce0977b74009c4500e00349554b
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2024-03-13 22:14:22 -0300

    initialCommit

---
+++ removecomma.py
@@ -0,0 +1,12 @@
+csv_file_path = 'steam_spy_data.csv'
+
+# Read the CSV file as text
+with open(csv_file_path, 'r') as file:
+    content = file.read()
+
+# Remove unwanted characters (e.g., semicolons)
+content_without_semicolons = content.replace(';', '')
+
+# Write the modified content back to the file
+with open(csv_file_path, 'w') as file:
+    file.write(content_without_semicolons)
